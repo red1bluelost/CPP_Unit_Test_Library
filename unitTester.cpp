772e4ba31f6046c9c7693e7fc8d6efae09e27dfc
red1bluelost <msweston12@cox.net>
2020-02-22 16:30:12 -0500

    Adds items tests for checking a single function in a larger unit test.

--- unitTester.cpp
+++ unitTester.cpp
@@ -7,4 +7,23 @@
 
 namespace r1bl {
 
+	bool UnitTester::funcCheck::itemTest::Result() {
+		if(!result) {
+			printf("%s\n", description);
+		}
+		return result;
+	}
+
+
+
+	void UnitTester::funcCheck::expect(const char * s, bool r) {
+		expects.push_back(itemTest(s,r));
+	}
+
+	bool UnitTester::funcCheck::executeCheck() {
+		for(auto it : expects) {
+			passState = it.Result() && passState;
+		}
+		return passState;
+	}
 }
--- unitTester.h
+++ unitTester.h
@@ -6,9 +6,64 @@
 #ifndef __UNIT_TESTER_H__
 #define __UNIT_TESTER_H__
 
+#include <cstdio>
+#include <vector>
 
 namespace r1bl {
+	
 
+	//Main class for the unit tester
+	class UnitTester {
+		//disable copy constructor, assignment operator, and default constructor
+		UnitTester( UnitTester & );
+		UnitTester operator = ( UnitTester & );
+		UnitTester();
+
+		const char * const unitTestTitle; //string of the unit test title
+		
+		unsigned long int passCount = 0;
+		unsigned long int failCount = 0;
+
+		public:
+		class funcCheck {
+			//disable assignment operator and default constructor
+			funcCheck operator = ( funcCheck & );
+			funcCheck();
+
+			//item test class to be used in a vector
+			class itemTest {
+				const char * description;
+				bool result;
+				//disable assignment operator and default constructor
+				itemTest operator = ( itemTest & );
+				itemTest ();
+
+				public:
+				itemTest (const char * s, bool r) : description(s) , result(r) {};
+				bool Result();
+
+			};
+
+			bool passState = true;
+			std::vector<itemTest> expects;
+
+
+			public:
+			void expect(const char *, bool);
+			bool executeCheck();
+
+		};
+
+		private:
+		std::vector<funcCheck> checks;
+
+		public:
+		void FuncCheck(const char *, void (funcCheck*) );
+
+	};
+	
+	
+	typedef UnitTester::funcCheck* CheckPtr;
 }
 
 #endif//__UNIT_TESTER_H__
