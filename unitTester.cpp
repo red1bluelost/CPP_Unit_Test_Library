2616db08a70fcb841aa175ffdb6d970e4f93c029
red1bluelost <msweston12@cox.net>
2020-02-22 18:23:52 -0500

    Improves in code documentation.

--- unitTester.cpp
+++ unitTester.cpp
@@ -6,6 +6,7 @@
 #include "unitTester.h"
 
 namespace r1bl {
+	//Returns if the expected result actually occurred
 	bool UnitTester::funcCheck::itemTest::Result() {
 		if(!result) {
 			printf("%s\n", description);
@@ -13,10 +14,12 @@ namespace r1bl {
 		return result;
 	}
 
+	//Adds a test within a function check that expects a specific result to be true
 	void UnitTester::funcCheck::Expect(const char * s, bool r) {
 		expects.emplace_back(new itemTest(s,r));
 	}
 
+	//Checks if all the expected results occurred in a single function test
 	bool UnitTester::funcCheck::executeCheck() {
 		for(auto & it : expects) {
 			passState = it->Result() && passState;
@@ -24,14 +27,18 @@ namespace r1bl {
 		return passState;
 	}
 
+	//Initializes a single function check
 	UnitTester::funcCheck::funcCheck(const char * s, void test(funcCheck*) ) : description(s) {
 		test(this);
 	}
 
+	//Creates a function test that takes a discription and a lambda function 
+	//containing user defined expectations
 	void UnitTester::FunctionTest(const char * s, void test(funcCheck*) ) {
 		checks.emplace_back(new funcCheck(s, test));
 	}
 
+	//Runs through each function check to see if everything passes and outputs the result
 	void UnitTester::ExecuteUnitTest() {
 		printf("%s\n\n", unitTestTitle);
 		for(auto & it : checks) {
--- unitTester.h
+++ unitTester.h
@@ -26,17 +26,21 @@ namespace r1bl {
 		unsigned long int failCount = 0;
 
 		public:
+		//Class to contain the tests for a single function/procedure
 		class funcCheck {
 			//disable copy constructor, assignment operator, and default constructor
 			funcCheck( funcCheck & );
 			funcCheck operator = ( funcCheck & );
 			funcCheck();
+
 			const char * const description;
 
 			//item test class to be used in a vector
+			//holds specific expectations for a function chect to have worked
 			class itemTest {
 				const char * const description;
 				bool result;
+
 				//disable copy constructor, assignment operator, and default constructor
 				itemTest( itemTest & );
 				itemTest operator = ( itemTest & );
@@ -45,27 +49,38 @@ namespace r1bl {
 				public:
 				itemTest (const char * s, bool r) : description(s) , result(r) {}
 				bool Result();
-
 			};
 
 			bool passState = true;
+
+			//vector holding all the expectation tests for a single function check
 			std::vector<std::unique_ptr<itemTest>> expects;
 
-
 			public:
-			void Expect(const char *, bool);
-			bool executeCheck();
-			const char * Describe() {return description;}
 			funcCheck(const char *, void (funcCheck*) );
 
+			const char * Describe() {return description;}
+
+			bool executeCheck();
+
+			//Used to define and check that specific results have occurred in the function
+			void Expect(const char *, bool);
 		};
 
 		private:
+		//vector containing all the function tests for a unit
 		std::vector<std::unique_ptr<funcCheck>> checks;
 
 		public:
+		//Three main methods to create a unit test
+		
+		//Initializes unit test object with a specific name
 		UnitTester(const char * s) : unitTestTitle(s) {}
+
+		//Creates tests that can be ran to verify the unit
 		void FunctionTest(const char *, void (funcCheck*) );
+
+		//Checks if all tests passed and outputs a summary
 		void ExecuteUnitTest();
 
 	};
